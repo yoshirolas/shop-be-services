882806da590ef72487af822fa85a9c27aea1f0c7
Alexey Otjukh <yoshirolas@tut.by>
2021-08-29 18:40:50 +0300

    chore(product-service): mock product data

--- product-service/product-mock-data/products.js
+++ product-service/product-mock-data/products.js
@@ -2,18 +2,21 @@ const products = [
     {
         id: '1',
         productName: 'Product 1',
+        title: 'Product 1',
         price: 100,
         currency: '$',
     },
     {
         id: '2',
         productName: 'Product 2',
+        title: 'Product 2',
         price: 100,
         currency: '$',
     },
     {
         id: '3',
         productName: 'Product 3',
+        title: 'Product 3',
         price: 100,
         currency: '$',
     },
