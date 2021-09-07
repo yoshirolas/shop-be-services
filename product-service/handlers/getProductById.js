853a926c8c93044dff3c060404657c6bdcf5ced2
Alexey Otjukh <yoshirolas@tut.by>
2021-09-07 21:37:32 +0300

    feat(product-service): connect db

--- product-service/handlers/getProductById.js
+++ product-service/handlers/getProductById.js
@@ -1,22 +1,35 @@
 'use strict';
 
-const { getProductById } = require('../products-data/utils');
+// const { getProductById } = require('../products-data/utils');
 const ApiError = require('../utils/apiError');
+const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
-// const corsHeaders = {
-//     headers: {
-//       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-//       'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
-//     }
-// };
+const { Client } = require('pg');
+const { PGHOST, PGUSER, PGDATABASE, PGPASSWORD, PGPORT } = process.env;
+const dbOption = {
+  user: PGUSER,
+  host: PGHOST,
+  database: PGDATABASE,
+  password: PGPASSWORD,
+  port: PGPORT,
+};
 
 module.exports.product = async (event) => {
+    const client = new Client(dbOption); 
+    client.connect();
+
     try {
         const { productId } = event.pathParameters;
         if (!productId) throw new ApiError(400, 'Error! Please use the following path structure: /product/{id}');
+        if (!productId.match(uuidRegex)) throw new ApiError(400, 'Wrong product id format');
 
-        const product = await getProductById(productId);
-        if (!product) throw new ApiError(404, `Error! Couldn't find product with id ${productId}`);
+        const { rows } = await client.query(
+            `select * from products p left join stocks s on p.id = s.product_id where p.id = '${productId}'`
+        );
+        if (rows.length !== 1) throw new ApiError(500, 'Wrong DB response format');
+
+        const product = rows[0];
+        if (!product) throw new ApiError(404, `Couldn't find product with id ${productId}`);
 
         return {
             statusCode: 200,
@@ -27,8 +40,10 @@ module.exports.product = async (event) => {
         
     } catch (error) {
         return {
-            statusCode: error.statusCode,
+            statusCode: error.statusCode || 500,
             body: error.message
         }; 
+    } finally {
+        client.end()
     }
 };
