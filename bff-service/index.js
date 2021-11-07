c5a3261a335c47f15da5f9daf441f3cfdf96abf9
Alexey Otjukh <yoshirolas@tut.by>
2021-11-07 13:44:41 +0300

    feat(bff-service): product service redirect

--- bff-service/index.js
+++ bff-service/index.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const axios = require('axios').default;
 
-app.get('/', (req, res) => {
-  res.send('start app');
+const services = [
+    {
+        serviceName: 'product-service',
+        servicePathIdentificator: 'products',
+        serviceUrl: 'https://ujhvl08zyb.execute-api.eu-west-1.amazonaws.com/dev'
+    }
+];
+
+app.use(express.json());
+
+app.all('*', async (req, res) => {
+    try {
+        const { method, path, body } = req;
+        // console.log(method)
+        // console.log(path)
+        // console.log(body)
+        const recipientService = services.find(service => service.servicePathIdentificator === path.split('/')[1]);
+        if (!recipientService) throw new Error();
+
+        const getUrl = () => `${recipientService.serviceUrl}${path}`;
+        const serviceResponse = await axios({
+            method: method,
+            url: getUrl(),
+            data: Object.keys(body).length > 0 ? body : null
+        });
+
+        res.send(JSON.stringify(serviceResponse.data));
+
+    } catch (error) {
+        const errorStatus = error.status || 502;
+        const errorMessage = error.message || 'Cannot process request';
+        res.status(errorStatus).send(errorMessage);
+    }
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 })
